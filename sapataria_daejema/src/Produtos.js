1ab9fce0bb232d9b1e28706cfe6c0f1531cecaec
37223 <matheusrocham.ms@gmai.com>
2024-11-22 17:35:04 -0300

    comeco_carrinho produto_tipagem

--- sapataria_daejema/src/Produtos.js
+++ sapataria_daejema/src/Produtos.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Dialog, DialogActions, DialogContent, DialogTitle, Grid2, TextField} from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { Box, Dialog, DialogActions, DialogContent, DialogTitle, Grid2, TextField } from "@mui/material";
 import { Card, CardContent, Typography, Button, Drawer, List, ListItem, ListItemText, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import {  theme } from "./static/Utils";
-import { ThemeProvider} from '@mui/material/styles';
-import {useBackground} from "./static/UseBackGround";
+import { theme } from "./static/Utils";
+import { ThemeProvider } from '@mui/material/styles';
+import { useBackground } from "./static/UseBackGround";
 import axios from 'axios';
 
 function Produtos() {
@@ -12,27 +12,53 @@ function Produtos() {
 
     const [drawerOpen, setDrawerOpen] = useState(false);
     const [produtos, setProdutos] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
 
+    // Função para carregar todos os produtos
     const fetchProducts = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/produto/produtos', {
+                headers: { "Content-Type": "application/json" }
+            });
+            setProdutos(response.data);
+        } catch (erro) {
+            console.log("Erro ao carregar os produtos: ", erro);
+        }
+    };
 
-        try{
-        const response = await axios.get('http://localhost:5000/produto/produtos',{
-        headers:{"Content-Type": "application/json"}
-        })
-        console.log(response.data)
-        setProdutos(response.data);
-    }catch (erro){
-            console.log("Erro ao carregar os produtos: ",erro);
+    // Função para carregar produtos filtrados por tipo
+    const fetchProductsByType = async (type) => {
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/produto/produtosTipo',
+                { type: type }, // Passando o tipo no corpo da requisição
+                {
+                    headers: { "Content-Type": "application/json" }
+                });
+            console.log("Produtos filtrados por tipo:", response.data);
+            setProdutos(response.data); // Atualizando a lista de produtos
+        } catch (erro) {
+            console.log("Erro ao carregar produtos por tipo: ", erro);
         }
     };
 
     useEffect(() => {
-        fetchProducts();
-    },[]);
+        fetchProducts(); // Carregar todos os produtos ao carregar a página
+    }, []);
 
     const toggleDrawer = (open) => {
         setDrawerOpen(open);
     };
+
+    const handleOpenModal = () => setOpenModal(true);
+    const handleCloseModal = () => setOpenModal(false);
+    const handleSaveProduct = async () => {
+    };
+
+    // Função para tratar o click nos tipos de produtos
+    const handleTypeClick = (type) => {
+        fetchProductsByType(type); // Filtra os produtos pelo tipo clicado
+    };
+
     return (
         <ThemeProvider theme={theme}>
             <IconButton onClick={() => toggleDrawer(true)} style={{ position: 'fixed', marginTop: '90px', left: '30px', cursor: 'pointer' }}>
@@ -48,27 +74,46 @@ function Produtos() {
                     flexShrink: 0,
                     '& .MuiDrawer-paper': {
                         width: 280,
-                        height:780,
-                        display:'flex',
+                        height: 780,
+                        display: 'flex',
                         marginTop: '65px',
                         backgroundColor: "solid",
-                        color:"black",
-                    }, }}
+                        color: "black",
+                    },
+                }}
             >
-
-                <List style={{margin:"50px 30px"}}>
-                    <ListItem button>
-                        <ListItemText primary="Tenis Futsal" />
+                <List style={{ margin: "50px 30px" }}>
+                    <ListItem button onClick={() => handleTypeClick('Tênis')}>
+                        <ListItemText primary="Tênis" />
                     </ListItem>
-                    <ListItem button>
-                        <ListItemText primary="Chuteira Campo" />
+                    <ListItem button onClick={() => handleTypeClick('Chuteira')}>
+                        <ListItemText primary="Chuteira" />
                     </ListItem>
-                    <ListItem button>
-                        <ListItemText primary="Sapato Social" />
+                    <ListItem button onClick={() => handleTypeClick('Bota')}>
+                        <ListItemText primary="Bota" />
+                    </ListItem>
+                    <ListItem button onClick={() => handleTypeClick('Chinelo')}>
+                        <ListItemText primary="Chinelo" />
                     </ListItem>
                 </List>
+                <Box style={{ marginTop: '120%' }}>
+                    <Button
+                        style={{
+                            background: '#FF8000',
+                            color: 'black',
+                            border: '2px solid black',
+                            borderRadius: '99px',
+                            width: 'max-content',
+                            margin: "30px",
+                        }}
+                        onClick={handleOpenModal}
+                    >
+                        Adicionar Mercadoria
+                    </Button>
+                </Box>
             </Drawer>
-            <Dialog>
+
+            <Dialog open={openModal} onClose={handleCloseModal}>
                 <DialogTitle>Cadastrar Nova Mercadoria</DialogTitle>
                 <DialogContent>
                     <TextField
@@ -106,56 +151,51 @@ function Produtos() {
                     />
                 </DialogContent>
                 <DialogActions>
-                    <Button
-                            color="secondary"
-                            variant="contained">
+                    <Button onClick={handleCloseModal} color="secondary" variant="contained">
                         Cancelar
                     </Button>
-                    <Button
-                        color="secundary"
-                        variant="contained"
-                    >
+                    <Button onClick={() => { handleCloseModal(); }} color="secondary" variant="contained">
                         Salvar
                     </Button>
                 </DialogActions>
             </Dialog>
 
             <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-            <Grid2 container spacing={2} width= "80%" >
-                {produtos.map((produto, index) => (
-                    <Grid2 item xs={12} sm={6} md={3} key={index}>
-                        <Card
-                            sx={{
+                <Grid2 container spacing={2} width="80%">
+                    {produtos.map((produto, index) => (
+                        <Grid2 item xs={12} sm={6} md={3} key={index}>
+                            <Card sx={{
                                 marginTop: '120px',
                                 border: '2px solid #02FF39',
                                 borderRadius: '24px',
-                            }}
-                        >
-                            <CardContent style={{ textAlign: 'center' }}>
-                                <Typography component="img" src={`http://localhost:5000${produto.url_img}`} alt={produto.nome_prod} sx={{ width: '100%', objectFit: 'cover' }} />
-                                <Typography variant="h5" style={{ fontWeight: 'bold' }}>
-                                    {produto.nome_prod}
-                                </Typography>
-                                <Typography variant="h5" style={{ fontWeight: 'bold' }}>
-                                    Valor Unitário: R${produto.preco_unit}
-                                </Typography>
-                                <Button
-                                    style={{
-                                        background: '#FF8000',
-                                        color: 'black',
-                                        border: '2px solid black',
-                                        borderRadius: '99px',
-                                    }}
-                                >
-                                    Comprar
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </Grid2>
-                ))}
-            </Grid2>
+                            }}>
+                                <CardContent style={{ textAlign: 'center' }}>
+                                    <Typography component="img" src={`http://localhost:5000${produto.url_img}`} alt={produto.nome_prod} sx={{ width: '100%', objectFit: 'cover' }} />
+                                    <Typography variant="h5" style={{ fontWeight: 'bold' }}>
+                                        {produto.nome_prod}
+                                    </Typography>
+                                    <Typography variant="h5" style={{ fontWeight: 'bold' }}>
+                                        Valor Unitário: R${produto.preco_unit}
+                                    </Typography>
+                                    <Button
+                                        style={{
+                                            background: '#FF8000',
+                                            color: 'black',
+                                            border: '2px solid black',
+                                            borderRadius: '99px',
+                                        }}
+                                    >
+                                        Comprar
+                                    </Button>
+                                </CardContent>
+                            </Card>
+                        </Grid2>
+                    ))}
+                </Grid2>
             </Box>
+
         </ThemeProvider>
     );
 }
+
 export default Produtos;
